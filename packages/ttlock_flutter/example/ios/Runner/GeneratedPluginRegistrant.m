f367ae17afcc7b1219dbb8b713ef4055699b448b
PasinduTY <pasindu.t.y.s@gmail.com>
2026-09-01 23:01:24 +0530

    Declare BLE scan/connect permissions in Android manifest

---
+++ packages/ttlock_flutter/example/ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,21 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<ttlock_flutter/TtlockFlutterPlugin.h>)
+#import <ttlock_flutter/TtlockFlutterPlugin.h>
+#else
+@import ttlock_flutter;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [TtlockFlutterPlugin registerWithRegistrar:[registry registrarForPlugin:@"TtlockFlutterPlugin"]];
+}
+
+@end
